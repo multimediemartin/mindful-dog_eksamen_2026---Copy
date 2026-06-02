88445cf6c8fd58418dd99dd5db18210aeea66204
multimediemartin <maso0013@stud.kea.dk>
2026-06-02 18:25:37 +0200

    forms template

--- eksamen-copy/env.d.ts
+++ eksamen-copy/env.d.ts
@@ -1,21 +1,10 @@
 /// <reference types="astro/client" />
 
-interface
- ImportMetaEnv {
-  
-readonly
- PUBLIC_SUPABASE_URL: 
-string
-;
-  
-readonly
- PUBLIC_SUPABASE_PUBLISHABLE_KEY: 
-string
-;
+interface ImportMetaEnv {
+  readonly PUBLIC_SUPABASE_URL: string;
+  readonly PUBLIC_SUPABASE_PUBLISHABLE_KEY: string;
 }
-interface
- ImportMeta {
-  
-readonly
- env: ImportMetaEnv;
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
